f5fee898e623228fd256c303e36a8ce6f81e1422
Foyaz-Ahmed <ahemdfoyaz0000@gmail.com>
2023-06-18 10:35:37 -0700

    declare list & function

--- practice.py
+++ practice.py
@@ -15,3 +15,29 @@ print(n)
 
 #all reserved keyword for pythorn
 print(keyword.kwlist)
+
+#type
+hello = 55.35
+print(type(hello))
+
+pi = 3.14
+print(type(pi))
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+a, b , _ = 1, 2, 3
+print(a, b)
+print(_)
+
+#list
+x = y = [10, 20, 20]
+print(x)
+z = [10, 20, [19, 20, 24], 33]
+print(z[2])
+
+#basic function declaration
+def my_function(x):
+    a = x + 5
+    return a
+print (my_function(10))
