ef684c094de9933086a7c3bd8495c84b45d268c1
Foyaz-Ahmed <ahemdfoyaz0000@gmail.com>
2023-06-18 10:16:27 -0700

    first-commit

---
+++ practice.py
@@ -0,0 +1,17 @@
+import keyword
+
+# different type variables declared
+print("Hello World")
+a = 123
+print(a)
+s = 'a'
+t = "Hello"
+print(s)
+print(t)
+m = True
+print(m)
+n = None
+print(n)
+
+#all reserved keyword for pythorn
+print(keyword.kwlist)
